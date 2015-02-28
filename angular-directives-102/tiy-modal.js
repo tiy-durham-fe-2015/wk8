3078844d72385683f9e423a32a02a0c30e657457
Chris Davies <chris.davies@theironyard.com>
2015-02-28 06:40:18 -0500

    more robust modal

--- angular-directives-102/tiy-modal.js
+++ angular-directives-102/tiy-modal.js
@@ -1,4 +1,5 @@
 app.directive('tiyModal', function () {
+
   return {
     restrict: 'E',
 
@@ -15,13 +16,40 @@ app.directive('tiyModal', function () {
     controller: ['$scope', function ($scope) {
       var self = this;
 
-      self.onclose = $scope.onclose;
-
       self.close = function () {
-        self.onclose();
+        $scope.onclose();
       };
     }],
 
-    controllerAs: 'vm'
+    controllerAs: 'vm',
+
+    link: function ($scope, element, attrs, ctrl) {
+
+      function closeModal () {
+        // This is how we tell Angular that we're about
+        // to change something. Since this event comes from
+        // a non-Angular source, we need to do this...
+        $scope.$apply(function() {
+          ctrl.close();
+        });
+      }
+
+      // We don't want to close the modal if we clicked
+      // inside the modal...
+      element[0].addEventListener('click', function (e) {
+        e.stopPropagation();
+      });
+
+      setTimeout(function () {
+        // We do want to close the modal if we click the document
+        document.addEventListener('click', closeModal);
+
+        // When the directive is destroyed, remove the
+        // click handler from document
+        $scope.$on('$destroy', function () {
+          document.removeEventListener('click', closeModal);
+        });
+      });
+    }
   }
 });
