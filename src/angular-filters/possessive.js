04e39618f10b9f55a64a7db31b175e75ee3a3954
Chris Davies <chris.davies@theironyard.com>
2015-02-28 11:13:50 -0500

    Adding comments to filters

--- src/angular-filters/possessive.js
+++ src/angular-filters/possessive.js
@@ -1,7 +1,17 @@
 app.filter('possessive', function() {
+  // This is the actual filter. It takes a single parameter,
+  // which is the value that we are going to modify. Whatever
+  // is returned by this function is what will be displayed
+  // (or passed along to the next filter in a filter chain).
   return function(input) {
+    // If input is null/undefined, make it an empty string
     input = input || '';
+
+    // If the last letter is an 's', just stick an apostrophe
+    // after it, otherwise, stick a 's after it
     var suffix = input.slice(-1).toLowerCase() === 's' ? '\'' : '\'s';
+
+    // Return the value we want to display
     return input + suffix;
   };
 });
--- src/angular-filters/with-role.js
+++ src/angular-filters/with-role.js
@@ -1,4 +1,6 @@
 app.filter('withRole', function () {
+  // Filters can take more than one parameter.
+  // In this case, we are passing in a role
   return function (items, role) {
     return (items || []).filter(function (person) {
       return person.role === role;
