89467da02d119be4539e57c3e8580cc100b3e714
Chris Davies <chris.davies@theironyard.com>
2015-02-28 09:26:05 -0500

    WebSocket demo

--- controllers/chats-controller.js
+++ controllers/chats-controller.js
@@ -1,6 +1,18 @@
 var app = require('../express-app'),
+    WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({ port: 8080 }),
     lastId = 0,
     chats = [];
+
+console.log('WebSockets on port 8080');
+
+// Web socket server shtuff
+wss.broadcast = function (data) {
+  var message = JSON.stringify(data);
+  wss.clients.forEach(function each(client) {
+    client.send(message);
+  });
+};
 
 // Routes for a silly image upload demonstration
 app.post('/chats', addChat);
@@ -10,6 +22,8 @@ function addChat (req, res) {
   var chat = req.body;
   chat.id = ++lastId;
   chats.push(chat);
+
+  wss.broadcast(chat);
 
   if (chats.length > 40) {
     chats.shift();
---
+++ src/angular-chat-socks/demo-controller.js
@@ -0,0 +1,42 @@
+app.controller('DemoCtrl', ['$http', '$scope', function ($http, $scope) {
+  var self = this;
+  // Create a web socket... This is "always on"... except
+  // when the network disconnects it for some reason.
+  // In that case, we'd have to manually detect the disconnect
+  // and attempt to reconnect... Also, in older browsers,
+  // WebSocket is not supported, so you need to detect that,
+  // and revert to polling!
+  //
+  // Finally, WebSocket is a browser feature which should
+  // definitely be hidden behind an angular factory so that
+  // it is injected into the controller...
+  var exampleSocket = new WebSocket('ws://localhost:8080');
+
+  self.username = '';
+  self.chatMessage = '';
+
+  // Fetch all messages from the server
+  $http.get('/chats').then(function (result) {
+    self.messages = result.data;
+  });
+
+  // Send a message to the server
+  self.sendMessage = function () {
+    // We'll just silently ignore errors!!!
+    $http.post('/chats', {
+      text: self.chatMessage,
+      user: self.username
+    });
+
+    self.chatMessage = '';
+  };
+
+  self.messages = [];
+
+  exampleSocket.onmessage = function (event) {
+    $scope.$apply(function () {
+      self.messages.push(JSON.parse(event.data));
+    });
+  };
+
+}]);
